9d6584746444ad403af0e6b9db4caad4f6ad4429
GnourtPrond97 <truonghhth1905001@fpt.edu.vn>
2020-08-19 19:09:12 +0700

    admin controller

---
+++ app/Http/Controllers/ControllerByAdmin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Customer;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ControllerByAdmin extends Controller
+{
+    public function listCustomers(){
+        $data =array();
+        $customers_list =Customer::query();
+
+        $data['list'] = $customers_list->get();
+        return view('admin.customers-manager')
+            ->with($data);
+    }
+}
