fc2e3194889161c96de028b429212cafcecc4b8a
Thái Anh <63459319+thaianhdo97@users.noreply.github.com>
2020-08-24 18:21:04 +0700

    Update 2020_08_13_140000_create_areas_table.php

--- database/migrations/2020_08_13_140000_create_areas_table.php
+++ database/migrations/2020_08_13_140000_create_areas_table.php
@@ -17,6 +17,7 @@ class CreateAreasTable extends Migration
             $table->id();
             $table->string('province');
             $table->string('region'); // Bắc, Trung, Nam
+            $table->integer('status');
             $table->timestamps();
         });
     }
